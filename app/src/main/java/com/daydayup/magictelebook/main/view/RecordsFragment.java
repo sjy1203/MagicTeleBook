895ada6a2f81bbb9671451a687023b21b5aad94c
hangyuyan <hangyuyan.x@gmail.com>
2016-05-09 10:55:21 +0800

    recordfragment1.0

--- app/src/main/java/com/daydayup/magictelebook/main/view/RecordsFragment.java
+++ app/src/main/java/com/daydayup/magictelebook/main/view/RecordsFragment.java
@@ -10,6 +10,12 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.daydayup.magictelebook.R;
+import com.daydayup.magictelebook.main.adpter.RecordAdapter;
+import com.daydayup.magictelebook.main.bean.Record;
+import com.daydayup.magictelebook.util.L;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -17,19 +23,35 @@ import com.daydayup.magictelebook.R;
 public class RecordsFragment extends Fragment {
     //view
     private RecyclerView recyclerView;
+    private List<Record> mRecords = new ArrayList<>();
+    private RecordAdapter mAapter;
+
     public RecordsFragment() {
         // Required empty public constructor
     }
-
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_records, container, false);
+        initRecords();
+        mAapter = new RecordAdapter(mRecords);
         recyclerView = (RecyclerView) view.findViewById(R.id.recyclerView);
         recyclerView.setLayoutManager(new LinearLayoutManager(container.getContext()));
+        recyclerView.setAdapter(mAapter);
+
         return view;
+    }
+
+    public void initRecords() {
+        L.d("initRecords");
+        Record[] record = new Record[10];
+        for(int i = 0;i < 10; i++){
+            record[i] = new Record("尚骏远","18986311211","来电","湖北武汉","16/05/08",
+                    R.mipmap.contact_type, R.mipmap.contact_area, R.mipmap.contact_time);
+            mRecords.add(record[i]);
+        }
     }
 
 }
